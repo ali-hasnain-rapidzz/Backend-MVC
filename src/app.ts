0bba1437eb3f7dff176b693e4d45eb0e39043690
sokainakanwal1 <sokainakanwal@rapidzzsolutions.com>
2024-08-06 15:57:57 +0500

    updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(rateLimiter);
 
 // routes
